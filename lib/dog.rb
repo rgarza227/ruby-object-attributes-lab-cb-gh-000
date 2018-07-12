89631e26beac34f78333e2d82fac9d97bc2476e8
Roberto Garza <robertg4567@hotmail.com>
2018-07-12 19:12:47 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,14 +4,14 @@ class Dog
     @name = name
     @breed = breed
   end
-
+  def name=(new_name)
+    @name = new_name
+  end
   def name
     @name
   end
 
-  def name=(new_name)
-    @name = new_name
-  end
+  
 
 
 
